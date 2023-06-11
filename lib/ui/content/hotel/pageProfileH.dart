64ba7c43fda37a83a2fad6b962eeb14ac1c7e832
jairozq <jairosalasvillazon@gmail.com>
2023-06-11 18:39:48 -0500

    modified:   lib/ui/content/hotel/pageProfileH.dart

--- lib/ui/content/hotel/pageProfileH.dart
+++ lib/ui/content/hotel/pageProfileH.dart
@@ -1,4 +1,7 @@
+import 'package:fasthotel/domain/controller/controllerHotel.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'dart:convert';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -8,8 +11,25 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  ControlHotel controlh = Get.put(ControlHotel());
+
+  decodeimage(imagen) {
+    var imagen64 = (imagen).split(",")[1];
+    var foto = base64Decode(imagen64);
+    return foto;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (controlh.listarHotels!.isEmpty) {
+      controlh.listHotels().then((value) => Get.toNamed("/homePageH"));
+    }
+    return Scaffold(
+      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        CircleAvatar(
+            backgroundImage:
+                MemoryImage(decodeimage(controlh.listarHotels![0].imagen))),
+      ]),
+    );
   }
 }
